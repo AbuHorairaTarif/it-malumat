51e3529004c805420ab9a31a5a65942216a96a6e
= <=>
2024-08-02 00:11:37 +0600

    dark-mode light-mode issue fixed

---
+++ assets/js/custom.js
@@ -0,0 +1,6 @@
+let mode = document.getElementById('mode');
+let bodyTag = document.getElementById("body");
+
+mode.addEventListener("click", function() {
+    bodyTag.classList.toggle("dark");
+});
